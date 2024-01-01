9a43d341bb4b06b1f0b9941b47873fa02b0a0555
Jacob Petterle <jacobpetterle@gmail.com>
2024-01-01 23:27:20 +0000

    test

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,7 +8,7 @@ const project = new typescript.TypeScriptProject({
   description: 'A CDK construct for Pinecone Indexes',
   projenrcTs: true,
   license: 'MIT',
-  copyrightOwner: 'Jacob Petterle',
+  copyrightOwner: 'Jacob Petterl',
   deps: [
     'projen@^0.75',
     'pnpm@^8.0',
