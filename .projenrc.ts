cf8ffc1ee056aceb22a027b6cf04950223bbcf1e
Jacob Petterle <jacobpetterle@gmail.com>
2024-01-20 21:35:33 +0000

    update deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -31,13 +31,14 @@ const library = new AwsCdkConstructLibrary({
   //   moduleName: 'github.com/petterle-endeavors/pinecone-db-construct',
   // },
   deps: [
-    'aws-cdk-lib@^2.118.0',
     '@aws-cdk/aws-lambda-python-alpha@^2.100.0-alpha.0',
-    'constructs@^10.0.5',
     'js-md5@^0.8',
   ],
   devDeps: [
     'projen@^0.75',
+  ],
+  bundledDeps: [
+    'js-md5@^0.8',
   ],
 });
 
